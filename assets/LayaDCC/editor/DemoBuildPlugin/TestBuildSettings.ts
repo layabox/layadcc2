53adf8545fc539a28f1b6eda9a6712d11b18dd9b
guozhaokui <guozhaokuiwang@gmail.com>
2024-04-11 17:50:24 +0800

    整理代码，加上dcc配置界面

--- assets/LayaDCC/editor/DemoBuildPlugin/TestBuildSettings.ts
+++ assets/LayaDCC/editor/DemoBuildPlugin/TestBuildSettings.ts
@@ -8,10 +8,76 @@ export class TestBuildSettings extends IEditor.EditorPanel {
                 catalogBarStyle : "hidden",
                 properties: [
                     {
-                        name: "enabled",
+                        name: "enable",
                         type: "boolean",
                         default: false
                     },
+                    {
+                        name: "desc",
+                        type: "string",
+                        default: "dcc update",
+                        tips:'本次版本描述',
+                        hidden: "!data.enable",
+                    },
+                    {
+                        name: "TargetPath",
+                        type: "string",
+                        default: Editor.projectPath+'/web/release/',
+                        hidden: "!data.enable",
+                    },
+                    {
+                        name: "OutputPath",
+                        type: "string",
+                        default: 'dccout',
+                        hidden: "!data.enable",
+                    },    
+                    {
+                        name: "OutputFile",
+                        type: "string",
+                        default: 'version',
+                        hidden: "!data.enable",
+                    },             
+                    {
+                        name: "version",
+                        type: "string",
+                        default: '1.0.0',
+                        hidden: "!data.enable",
+                    },                           
+                    {
+                        name: "ReserveOldAssets",
+                        type: "boolean",
+                        default: true,
+                        hidden: "!data.enable",
+                    },               
+                    {
+                        name: "FastMode",
+                        type: "boolean",
+                        default: true,
+                        hidden: "!data.enable",
+                    },                       
+                    {
+                        name: "mergeSmallFiles",
+                        type: "boolean",
+                        default: false,
+                        hidden: "!data.enable",
+                    },                
+                    {
+                        name: "MaxSmallFileSize",
+                        caption:"Max small file size (K)",
+                        addIndent: 1,
+                        type: "number",
+                        default: 100,
+                        hidden: "!(data.mergeSmallFiles&&data.enable)",
+                    },                
+                    {
+                        name: "MaxPackSize",
+                        caption:"Max pack size (K)",
+                        addIndent: 1,
+                        type: "number",
+                        default: 1000,
+                        hidden: "!(data.mergeSmallFiles&&data.enable)",
+                    },                
+
                 ]
             }
         ]);
