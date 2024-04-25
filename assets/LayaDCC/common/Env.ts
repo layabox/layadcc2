a1438c5c262447bec05da58295f06c27a9d2a411
guozhaokui <guozhaokuiwang@gmail.com>
2024-04-25 19:39:08 +0800

    dcc不能依赖laya

--- assets/LayaDCC/common/Env.ts
+++ assets/LayaDCC/common/Env.ts
@@ -14,11 +14,54 @@ export class Env{
     static dcodeUtf8(buf:ArrayBuffer){
         if(window.conch){
             //return conch.bufferToString(buf);
-            let buff = new Laya.Byte(buf);
-            let str = buff.readUTFBytes(buf.byteLength);
+            let str = decodeBuffer(buf);
             return str;
         }else{
             return (new TextDecoder()).decode(buf);
         }
     }
+}
+
+
+function decodeBuffer(buf:ArrayBuffer): string {
+    let len = buf.byteLength;
+    let pos = 0;
+    var v = "", max = len, c: number, c2: number, c3: number, f = String.fromCharCode;
+    var u = new Uint8Array(buf), i = 0;
+    var strs: any[] = [];
+    var n = 0;
+    strs.length = 1000;
+    while (pos < max) {
+        c = u[pos++];
+        if (c < 0x80) {
+            if (c != 0)
+                //v += f(c);\
+                strs[n++] = f(c);
+        } else if (c < 0xE0) {
+            //v += f(((c & 0x3F) << 6) | (u[_pos_++] & 0x7F));
+            strs[n++] = f(((c & 0x3F) << 6) | (u[pos++] & 0x7F));
+        } else if (c < 0xF0) {
+            c2 = u[pos++];
+            //v += f(((c & 0x1F) << 12) | ((c2 & 0x7F) << 6) | (u[_pos_++] & 0x7F));
+            strs[n++] = f(((c & 0x1F) << 12) | ((c2 & 0x7F) << 6) | (u[pos++] & 0x7F));
+        } else {
+            c2 = u[pos++];
+            c3 = u[pos++];
+            //v += f(((c & 0x0F) << 18) | ((c2 & 0x7F) << 12) | ((c3 << 6) & 0x7F) | (u[_pos_++] & 0x7F));
+            const _code = ((c & 0x0F) << 18) | ((c2 & 0x7F) << 12) | ((c3 & 0x7F) << 6) | (u[pos++] & 0x7F);
+            if (_code >= 0x10000) {
+                const _offset = _code - 0x10000;
+                const _lead = 0xd800 | (_offset >> 10);
+                const _trail = 0xdc00 | (_offset & 0x3ff);
+                strs[n++] = f(_lead);
+                strs[n++] = f(_trail);
+            }
+            else {
+                strs[n++] = f(_code);
+            }
+        }
+        i++;
+    }
+    strs.length = n;
+    return strs.join('');
 }
