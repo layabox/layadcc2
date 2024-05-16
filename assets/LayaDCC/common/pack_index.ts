cc1a1a49a41bc1640af9df4e2dda86cdf2c79866
guozhaokui <guozhaokuiwang@gmail.com>
2024-05-17 00:00:53 +0800

    接管下载放到用户那里

--- assets/LayaDCC/common/pack_index.ts
+++ assets/LayaDCC/common/pack_index.ts
@@ -3,14 +3,14 @@ import { LayaDCCClient } from "./LayaDCCClient";
 export { LayaDCCClient };
 
 //conch
-(async ()=>{
-if(window.conch){
-    let dcc = new LayaDCCClient('http://localhost:7788/' );
-    //设置这个地址下的资源加载走DCC模式
-    dcc.pathMapToDCC= 'http://localhost:8899/';
-    //通过DCC的根文件初始化dcc客户端
-    let initok = await dcc.init('http://localhost:7788/version.1.0.0.json',null);
+// (async ()=>{
+// if(window.conch){
+//     let dcc = new LayaDCCClient('http://localhost:7788/' );
+//     //设置这个地址下的资源加载走DCC模式
+//     dcc.pathMapToDCC= 'http://localhost:8899/';
+//     //通过DCC的根文件初始化dcc客户端
+//     let initok = await dcc.init('http://localhost:7788/version.1.0.0.json',null);
 
-    dcc.injectToNative3();
-}
-})();
+//     dcc.injectToNative3();
+// }
+// })();
