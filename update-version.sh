b96df1391ebb90e148377e5a6a3629bc6e2d828f
guozhaokui <guozhaokuiwang@gmail.com>
2024-10-29 22:56:04 +0800

    v 1.3.5

--- update-version.sh
+++ update-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VER='1.3.3'
+VER='1.3.5'
 sed -i 's/"version": ".*"/"version": "'$VER'"/' package.json
 sed -i "s/\.version('.*')/\.version('$VER')/" cli/bin.ts
 #git add package.json cli/bin.ts
